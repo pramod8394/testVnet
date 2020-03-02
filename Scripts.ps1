8fa5e3ce39cf381ef785da4ca03ee5d1123a2d90
pramod8394 <53146805+pramod8394@users.noreply.github.com>
2020-03-02 19:06:05 +0530

    ppp

--- Scripts.ps1
+++ Scripts.ps1
@@ -1,1 +1,1 @@
-Invoke-Gherkin -Script "_pramod8394/testVnet/demo*" -OutputFile "./Test-Pester.XML" -OutputFormat 'NUnitXML'
+Invoke-Gherkin -Script "$(System.DefaultWorkingDirectory)/_pramod8394_testVnet/demo*" -OutputFile "./Test-Pester.XML" -OutputFormat 'NUnitXML'

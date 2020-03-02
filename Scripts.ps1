40caa4d35ff357f150ce4cbd30607cc1f67d27d7
pramod8394 <53146805+pramod8394@users.noreply.github.com>
2020-03-02 19:01:13 +0530

    as

--- Scripts.ps1
+++ Scripts.ps1
@@ -1,1 +1,1 @@
-Invoke-Gherkins -Script "_pramod8394/testVnet/demo*" -OutputFile "./Test-Pester.XML" -OutputFormat 'NUnitXML' -CodeCoverage "./LSECosmos/*.ps1"
+Invoke-Gherkin -Script "_pramod8394/testVnet/demo*" -OutputFile "./Test-Pester.XML" -OutputFormat 'NUnitXML' -CodeCoverage "./LSECosmos/*.ps1"

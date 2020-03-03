770a631ded26857ea0ac9f5a0bebcb288ac1be3d
pramod8394 <53146805+pramod8394@users.noreply.github.com>
2020-03-03 10:56:39 +0530

    added crednetials

--- Scripts.ps1
+++ Scripts.ps1
@@ -1,1 +1,2 @@
+Connect-AzAccount -Tenant "927ede5c-2f25-4481-bbb1-b2d98307c0aa" -SubscriptionId "b59393fe-37a8-4e8c-880e-d7c55234f362"
 Invoke-Gherkin -Script "*pramod8394/testVnet/demo*" -OutputFile "./Test-Pester.XML" -OutputFormat 'NUnitXML'

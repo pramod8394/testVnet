78a6a9428ff5f8f5383382b93682428af7ced26c
pramod8394 <53146805+pramod8394@users.noreply.github.com>
2020-03-03 11:36:23 +0530

    jhghj

--- Scripts.ps1
+++ Scripts.ps1
@@ -1,2 +1,3 @@
-Connect-AzAccount -Tenant "927ede5c-2f25-4481-bbb1-b2d98307c0aa" -SubscriptionId "b59393fe-37a8-4e8c-880e-d7c55234f362"
+Install-Module -Name "Pester" -Force -SkipPublisherCheck
+Import-Module Pester
 Invoke-Gherkin -Script "*pramod8394/testVnet/demo*" -OutputFile "./Test-Pester.XML" -OutputFormat 'NUnitXML'
